c5daa7cae79ca4e64bbc4cd7c9731a034ad48271
pgm-stijwalr <147693656+pgm-stijwalr@users.noreply.github.com>
2024-10-17 13:40:17 +0200

    added bootstrap

--- views/_layout/main.php
+++ views/_layout/main.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $_ENV['SITE_NAME'] . ' ' . '|' . ' ' . ($title ?? '') . ' '  ?></title>
     <link rel="stylesheet" href="/css/main.css?v=<?php if( $_ENV['DEV_MODE'] == "true" ) { echo time(); }; ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
-    <nav>
+    <nav class="navbar">
         <a href="/">Home</a>
         <a href="#">item 2</a>
         <a href="#">item 3</a>
@@ -22,5 +24,7 @@
     <footer>
         &copy; <?= date('Y'); ?> - BrandName
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
 </html>
